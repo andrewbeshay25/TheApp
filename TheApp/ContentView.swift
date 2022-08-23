49b659d0d87640e4af4456236b0533954f677e27
Andrew <andrewbeshay2525@gmail.com>
2022-08-22 20:41:42 -0400

    Initial Commit

---
+++ TheApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheApp
+//
+//  Created by Andrew Beshay on 8/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheApp/TheAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheAppApp.swift
+//  TheApp
+//
+//  Created by Andrew Beshay on 8/22/22.
+//
+
+import SwiftUI
+
+@main
+struct TheAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
